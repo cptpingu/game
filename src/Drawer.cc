95ce12897346cd1e65bcf4c21829acc656a8600c
cptpingu <cptpingu@gmail.com>
2012-01-25 20:51:06 +0100

    Re-add the Drawer.cc

--- src/Drawer.cc
+++ src/Drawer.cc
@@ -1,3 +1,4 @@
+
 #include "Map.hh"
 #include "Drawer.hh"
 #include "SolTriangle.hh"
@@ -9,7 +10,6 @@
 #include <time.h>
 #include "Vector3D.hh"
 
-
 //Le fonctionnement idéal du drawer => il recoit des listes d'objets typés et dessine les objets OPEN_GL correspondants
 
 namespace
@@ -18,93 +18,79 @@ void draw(const Map::blocks_type::const_iterator& from,
           const Map::blocks_type::const_iterator& to)
 
 {
-
-
-    glBegin(GL_QUADS);
-
     TextureManager& textures = Singleton<TextureManager>::getInstance();
-
     glBindTexture(GL_TEXTURE_2D, textures["brick1"]);
-
-
 
     for (Map::blocks_type::const_iterator it = from; it != to; ++it)
     {
+        glPushMatrix();
+        glTranslatef((*it)->_x * (*it)->_size, (*it)->_y * (*it)->_size, (*it)->_z * (*it)->_size);
+        glBegin(GL_QUADS);
 
+        //par terre
+        glTexCoord2d(0, 0);
+        glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2,0);
+        glTexCoord2d((*it)->_size, 0);
+        glVertex3d((*it)->_size / 2, -(*it)->_size / 2,0);
+        glTexCoord2d((*it)->_size, (*it)->_size);
+        glVertex3d((*it)->_size / 2, (*it)->_size / 2,0);
+        glTexCoord2d(0, (*it)->_size);
+        glVertex3d(-(*it)->_size / 2, (*it)->_size / 2,0);
 
+        //face droite
+        glTexCoord2d(0, 0);
+        glVertex3d(-(*it)->_size / 2,-(*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, 0);
+        glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, (*it)->_size);
+        glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, 0);
+        glTexCoord2d(0, (*it)->_size);
+        glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2, 0);
 
-    glTranslatef((*it)->_x * (*it)->_size, (*it)->_y * (*it)->_size, (*it)->_z * (*it)->_size);
-    glPushMatrix();
+        //face gauche
+        glTexCoord2d(0, 0);
+        glVertex3d((*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, 0);
+        glVertex3d((*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, (*it)->_size);
+        glVertex3d((*it)->_size / 2, (*it)->_size / 2, 0);
+        glTexCoord2d(0, (*it)->_size);
+        glVertex3d((*it)->_size / 2, -(*it)->_size / 2, 0);
 
-    //par terre
-    glTexCoord2d(0, 0);
-    glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2,0);
-    glTexCoord2d((*it)->_size, 0);
-    glVertex3d((*it)->_size / 2, -(*it)->_size / 2,0);
-    glTexCoord2d((*it)->_size, (*it)->_size);
-    glVertex3d((*it)->_size / 2, (*it)->_size / 2,0);
-    glTexCoord2d(0, (*it)->_size);
-    glVertex3d(-(*it)->_size / 2, (*it)->_size / 2,0);
+        //face face
+        glTexCoord2d(0, 0);
+        glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, 0);
+        glVertex3d((*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, (*it)->_size);
+        glVertex3d((*it)->_size / 2, (*it)->_size / 2,0);
+        glTexCoord2d(0, (*it)->_size);
+        glVertex3d(-(*it)->_size / 2, (*it)->_size / 2,0);
 
-    //face droite
-    glTexCoord2d(0, 0);
-    glVertex3d(-(*it)->_size / 2,-(*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, 0);
-    glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, (*it)->_size);
-    glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, 0);
-    glTexCoord2d(0, (*it)->_size);
-    glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2, 0);
+        //face derriere
+        glTexCoord2d(0, 0);
+        glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, 0);
+        glVertex3d((*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, (*it)->_size);
+        glVertex3d((*it)->_size / 2, -(*it)->_size / 2, 0);
+        glTexCoord2d(0, (*it)->_size);
+        glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2,0);
+        //face au ciel
+        glTexCoord2d(0, 0);
+        glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, 0);
+        glVertex3d((*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
+        glTexCoord2d((*it)->_size, (*it)->_size);
+        glVertex3d((*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
+        glTexCoord2d(0, (*it)->_size);
+        glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
 
-    //face gauche
-    glTexCoord2d(0, 0);
-    glVertex3d((*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, 0);
-    glVertex3d((*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, (*it)->_size);
-    glVertex3d((*it)->_size / 2, (*it)->_size / 2, 0);
-    glTexCoord2d(0, (*it)->_size);
-    glVertex3d((*it)->_size / 2, -(*it)->_size / 2, 0);
+        glEnd();
 
-    //face face
-    glTexCoord2d(0, 0);
-    glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, 0);
-    glVertex3d((*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, (*it)->_size);
-    glVertex3d((*it)->_size / 2, (*it)->_size / 2,0);
-    glTexCoord2d(0, (*it)->_size);
-    glVertex3d(-(*it)->_size / 2, (*it)->_size / 2,0);
-
-    //face derriere
-    glTexCoord2d(0, 0);
-    glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, 0);
-    glVertex3d((*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, (*it)->_size);
-    glVertex3d((*it)->_size / 2, -(*it)->_size / 2, 0);
-    glTexCoord2d(0, (*it)->_size);
-    glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2,0);
-    //face au ciel
-    glTexCoord2d(0, 0);
-    glVertex3d(-(*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, 0);
-    glVertex3d((*it)->_size / 2, -(*it)->_size / 2, (*it)->_size);
-    glTexCoord2d((*it)->_size, (*it)->_size);
-    glVertex3d((*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
-    glTexCoord2d(0, (*it)->_size);
-    glVertex3d(-(*it)->_size / 2, (*it)->_size / 2, (*it)->_size);
-
-    glPopMatrix();
-    glEnd();
-
+        glPopMatrix();
     }
-
-
-
 }
-
-
 
 void draw(const Map::solTriangle_type::const_iterator& from,
           const Map::solTriangle_type::const_iterator& to)
@@ -114,38 +100,24 @@ void draw(const Map::solTriangle_type::const_iterator& from,
 
     TextureManager& textures = Singleton<TextureManager>::getInstance();
 
-    //glBindTexture(GL_TEXTURE_2D, textures["test"]);
-
+    glBindTexture(GL_TEXTURE_2D, textures["test"]);
 
     glBegin(GL_TRIANGLE_STRIP);
-    int k = 0;
-glColor3ub(0,0,255);
-    for (Map::solTriangle_type::const_iterator it = from; it != to; ++it)
-       { k=k+1;
-        glVertex3d((*it)->_x,(*it)->_y,(*it)->_z);
-    if (k ==25){glColor3ub(255,0,0);}
 
-    if (k==50){glColor3ub(0,0,255);}
-     k=k%51;
-    }
+    for (Map::solTriangle_type::const_iterator it = from; it != to; ++it)
+        glVertex3d((*it)->_x,(*it)->_y,(*it)->_z);
 
     glEnd();
     glPopMatrix();
 }
 
-
-}
-
-
-
+} // namespace
 
 Drawer::Drawer()
 {
 }
 
-
 void Drawer::drawMap(const Map & map) const
-
 {
     draw(map.begin(), map.end());
     draw(map.Tbegin(), map.Tend());

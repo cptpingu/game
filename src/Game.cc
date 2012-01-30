1c965f2da63e9e084ec522cf14a3b9dbeb1a18a6
cptpingu <cptpingu@gmail.com>
2012-01-30 23:48:04 +0100

    Clean the project, removing useless files

--- src/Game.cc
+++ src/Game.cc
@@ -10,7 +10,7 @@ Game::load()
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_TEXTURE_2D);
 
-  chargerTextures();
+  loadtextures();
   static const std::string filename = "Carte.txt";
   if (!_map.loadBlocks(filename))
   {
@@ -85,6 +85,46 @@ Game::play()
     if ((stop_time - last_time) < time_per_frame)
       SDL_Delay(time_per_frame - (stop_time - last_time));
   }
+}
+
+void
+Game::loadtextures()
+{
+
+  TextureManager& textures = Singleton<TextureManager>::getInstance();
+  //TextureManager& textures = TextureManager::getInstance();
+
+  textures.load("data/images/metal091.jpg", "metal");
+  textures.load("data/images/rocket_motor.jpg", "rocketMotor");
+  textures.load("data/images/rocket_bottom.jpg", "rocketBottom");
+  textures.load("data/images/rocket_middle.jpg", "rocketMiddle");
+  textures.load("data/images/rocket_top.jpg", "rocketTop");
+  //autre texture
+  textures.load("data/images/concrete001.jpg", "concrete");
+  textures.load("data/images/neige.jpg", "neige");
+  textures.load("data/images/floor032.jpg", "floor1");
+  textures.load("data/images/brick009.jpg", "brick1");
+  textures.load("data/images/wood002.jpg", "wood1");
+  textures.load("data/images/wood006.jpg", "wood2");
+  //porte
+  textures.load("data/images/door009.jpg", "door");
+  //pour les caisse
+  textures.load("data/images/crate03.jpg", "crate");
+  //toit
+  textures.load("data/images/roof05.jpg", "roof");
+  //fenetre
+  textures.load("data/images/window031_alpha.png", "window");
+  //Herbe
+  textures.load("data/images/veg008.jpg", "veg1");
+  //Eau de la piscine
+  textures.load("data/images/eau.jpg", "eau");
+  // Dalle de la piscine
+  textures.load("data/images/floor006b.jpg", "floor2");
+  // Fond du puit
+  textures.load("data/images/veg010.jpg", "veg2");
+  // puit
+  textures.load("data/images/brick077.jpg", "brick2");
+  textures.load("data/images/test.jpg", "test");
 }
 
 void
--- src/Scene.hh
+++
@@ -1,7 +0,0 @@
-#ifndef SCENE_HH_
-# define SCENE_HH_
-
-void chargerTextures();
-
-
-#endif /* !SCENE_HH_ */

a027795aacb25be708ffad26d0c1ed76697ae1cc
CptPingu <cptpingu@gmail.com>
2012-02-19 18:35:18 +0100

    Make it works on Linux.

--- src/Random.hh
+++ src/Random.hh
@@ -5,10 +5,13 @@
 
 namespace Random
 {
+# ifdef _WIN32
   typedef std::mersenne_twister<unsigned int, 32, 624,
           397, 31, 0x9908b0df, 11, 7, 0x9d2c5680,
           15, 0xefc60000, 18> mersenne_twister;
-
+# else
+  typedef std::mt19937 mersenne_twister;
+# endif
   unsigned int rand();
 }
 
